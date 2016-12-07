a4e7962d2d02059b77f3ea054b4daedf29742c76
Lucas Giusti <giusti.lucas@gmail.com>
2016-12-07 15:08:38 -0200

    add files

---
+++ server.js
@@ -0,0 +1,6 @@
+﻿var mosca = require('mosca')
+var server = new mosca.Server({ port: 1883, maxConnections: 1000 });
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
